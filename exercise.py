a1d3336f779afaaa0f2dd1c22eb9a0ab35cb9176
NolanEliason <78878336+NolanEliason@users.noreply.github.com>
2021-03-28 22:13:19 -0500

    CursorQueryResults
    added cursor variable from connection, execute a query and store the info in query variable, fetch and print all

--- exercise.py
+++ exercise.py
@@ -6,3 +6,10 @@ password ="ardit700_student",
 host = "108.167.140.122",
 database = "ardit700_pm1database"
 )
+
+cursor = con.cursor()
+
+query = cursor.execute("SELECT * FROM Dictionary ")
+results = cursor.fetchall()
+
+print(results)
